f8432da54f6bd31e63d915e387bae44a4d1154ae
Bodula-sravani <sravanibodula@gmail.com>
2023-04-07 11:35:54 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Data.SqlClient;
+namespace CricketTorunment
+{
+    internal class CollegeSportsManagementSystem
+    {
+        public string connectionString = "Data Source=DESKTOP-8C83AOT;Initial Catalog=CollegeSportsTorunments;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        SqlConnection connection = new SqlConnection();
+
+       
+
+        public void createSportsTable()
+        {
+            using (SqlCommand command = connection.CreateCommand())
+            {
+                command.CommandText = "create table Sports(Id int primary key,Name varchar(255) not null);";
+                command.ExecuteNonQuery();
+            }
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            CollegeSportsManagementSystem c = new();
+            c.connection = new SqlConnection(c.connectionString);
+            c.connection.Open();
+            c.createSportsTable();
+            c.connection.Close();
+
+        }
+    }
+}
