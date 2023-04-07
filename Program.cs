268a83c11c6f970d9d0dc0129880ff06d4e09072
Bodula-sravani <sravanibodula@gmail.com>
2023-04-07 11:52:39 +0530

    add tournments,teams,players tables

--- Program.cs
+++ Program.cs
@@ -16,7 +16,48 @@ namespace CricketTorunment
                 command.ExecuteNonQuery();
                 Console.WriteLine("Sports Table created");
             }
+        }
 
+        public void CreateTournmentsTable()
+        {
+            using (SqlCommand command = connection.CreateCommand())
+            {
+                command.CommandText = "create table Tournments(Id int primary key,Name varchar(255) not null,sports_id int not null," +
+                    "foreign key(sports_id) REFERENCES Sports(Id)" +
+                    "on delete cascade);";
+                command.ExecuteNonQuery();
+                Console.WriteLine("Tournments Table created");
+            }
+
+        }
+
+        public void CreateTeamsTable()
+        {
+            using (SqlCommand command = connection.CreateCommand())
+            {
+                command.CommandText = "create table Teams(Id int primary key,Name varchar(255) not null,tournment_id int not null," +
+                    "foreign key(tournment_id) REFERENCES Tournments(Id)" +
+                    "on delete cascade);";
+                command.ExecuteNonQuery();
+                Console.WriteLine("Teams Table created");
+
+
+            }
+
+        }
+
+        public void CreatePlayersTable() 
+        {
+           using(SqlCommand command = connection.CreateCommand())
+            {
+                command.CommandText = "create table Players(Id int primary key,Name varchar(255) not null,team_id int not null," +
+                    "foreign key(team_id) REFERENCES Teams(Id)" +
+                    "on delete cascade);";
+                command.ExecuteNonQuery();
+                Console.WriteLine("Players Table created");
+
+
+            }
 
         }
 
@@ -26,7 +67,10 @@ namespace CricketTorunment
             CollegeSportsManagementSystem c = new();
             c.connection = new SqlConnection(c.connectionString);
             c.connection.Open();
-            c.createSportsTable();
+            //c.createSportsTable();
+            //c.CreateTournmentsTable();
+            c.CreateTeamsTable();
+            c.CreatePlayersTable();
             c.connection.Close();
 
         }
